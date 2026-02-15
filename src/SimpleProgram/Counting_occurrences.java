a8f5a469d7595255d48cb66a6263484895b2fa13
chetanmalage <chetanmalage234@gmail.com>
2026-02-15 16:02:26 +0530

    Counting_occurrences program added.

---
+++ src/SimpleProgram/Counting_occurrences.java
@@ -0,0 +1,21 @@
+package SimpleProgram;
+
+import java.util.Scanner;
+
+public class Counting_occurrences {
+    static void main(String[] args) {
+
+        int n = 44252525;
+
+        int count = 0;
+
+        while (n > 0) {
+            int lastDigit = n%10;
+            if (lastDigit == 5){
+                count++;
+            }
+            n = n/10;
+        }
+        System.out.println(count);
+    }
+}
