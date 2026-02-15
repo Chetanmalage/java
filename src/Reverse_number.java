c52e22bf6b2a86bb1b58d685e5e1bb370c621a8d
chetanmalage <chetanmalage234@gmail.com>
2026-02-15 16:07:41 +0530

    ReveseNumber program added.

---
+++ src/Reverse_number.java
@@ -0,0 +1,14 @@
+public class Reverse_number {
+    public static void main(String[] args) {
+        int n = 123456;
+
+        int ans = 0;
+
+        while (n > 0) {
+            int lastDigit = n % 10;
+            ans = ans * 10 + lastDigit;
+            n = n / 10;
+        }
+        System.out.println(ans);
+    }
+}
