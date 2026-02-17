553ff55ec765eabddba89e028d435b6fadf057bb
chetanmalage <chetanmalage234@gmail.com>
2026-02-17 20:34:16 +0530

    functions programs added.

--- src/Functions/PrimeNumberRange.java
+++ src/Functions/PrimeNumberRange.java
@@ -3,7 +3,7 @@ package Functions;
 import java.util.Scanner;
 
 public class PrimeNumberRange {
-//    Write a function that returns all prime numbers between two given numbers.
+    //    Write a function that returns all prime numbers between two given numbers.
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter a Starting number: ");
@@ -11,13 +11,12 @@ public class PrimeNumberRange {
         System.out.print("Enter a Ending number: ");
         int end = input.nextInt();
 
-        printPrimes(start,end);
+        printPrimes(start, end);
     }
 
 
-
     public static boolean isPrime(int n) {
-        if (n <= 1){
+        if (n <= 1) {
             return false;
         }
 
@@ -31,10 +30,10 @@ public class PrimeNumberRange {
 
 
     static void printPrimes(int start, int end) {
-    for (int i = start; i <= end; i++) {
-        if (isPrime(i)) {
-            System.out.print(i + " ");
+        for (int i = start; i <= end; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
         }
     }
-}
 }
